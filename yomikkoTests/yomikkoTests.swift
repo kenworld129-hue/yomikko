ef4705e1d8dab5283c466ff6a67ebccea333dfa4
kenworld129-hue <240153182+kenworld129-hue@users.noreply.github.com>
2026-04-21 22:21:12 +0900

    Initial Commit

---
+++ yomikkoTests/yomikkoTests.swift
@@ -0,0 +1,17 @@
+//
+//  yomikkoTests.swift
+//  yomikkoTests
+//
+//  Created by kenshun on 2026/04/21.
+//
+
+import Testing
+@testable import yomikko
+
+struct yomikkoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
